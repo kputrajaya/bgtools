8262df58afa616a9af84df286374645fd2d5fcf7
Kevin Putrajaya <freakperzon@yahoo.com>
2023-01-02 08:43:11 +0700

    Show top 1000

--- site.js
+++ site.js
@@ -10,71 +10,79 @@ document.addEventListener('alpine:init', () => {
   setInterval(relayout, 1000);
 
   Alpine.data('bgs', () => ({
+    // Data
     username: '',
     items: null,
     loading: false,
-    init() {
-      const params = new Proxy(new URLSearchParams(window.location.search), {
-        get: (searchParams, prop) => searchParams.get(prop),
-      });
-      this.username = params.u || '';
-      if (!this.username) return;
+    limit: 1000,
 
+    // Computed
+    itemsLength() {
+      return (this.items || []).length;
+    },
+    itemsSliced() {
+      return (this.items || []).slice(0, this.limit);
+    },
+
+    // Method
+    init() {
+      const params = new URLSearchParams(window.location.search);
+      this.username = params.get('u') || '';
+      if (!this.username) return;
       this.load();
+    },
+    formatNumber(num) {
+      return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
     },
     async load() {
       this.loading = true;
 
-      // Initiate constants
-      const host = 'https://boardgamegeek.com/xmlapi';
-      const options = { 'Content-Type': 'application/xml' };
-      const parser = new XMLParser({ ignoreAttributes: false });
-
       // Get collection data and retry
       let resCollection = null;
-      while (!resCollection) {
-        resCollection = await fetch(host + '/collection/' + this.username, options);
-        if (resCollection.status !== 200) {
-          resCollection = null;
-          await new Promise((resolve) => setTimeout(resolve, 2000));
-        }
+      const url = 'https://boardgamegeek.com/xmlapi/collection/' + this.username + '?own=1';
+      const options = { 'Content-Type': 'application/xml' };
+      while (true) {
+        resCollection = await fetch(url, options).catch(() => null);
+        if (resCollection && resCollection.status === 200) break;
+        await new Promise((resolve) => setTimeout(resolve, 2000));
       }
 
       // Parse collection data
       const collectionText = await resCollection.text();
+      const parser = new XMLParser({ ignoreAttributes: false });
       const collectionObj = parser.parse(collectionText);
-      const collection = collectionObj.items.item
-        .map((item) => ({
-          id: item['@_objectid'],
-          image: item.image,
-          thumbnail: item.thumbnail,
-          name: String(item.name['#text']),
-          year: item.yearpublished,
-          players: {
-            min: Math.floor(item.stats['@_minplayers']),
-            max: Math.floor(item.stats['@_maxplayers']),
-          },
-          playTime: {
-            min: Math.floor(item.stats['@_minplaytime']),
-            max: Math.floor(item.stats['@_maxplaytime']),
-          },
-          rating: Math.floor(item.stats.rating['@_value']) || null,
-          ratingBgg: (Math.round(parseFloat(item.stats.rating.average['@_value']) * 10) / 10).toFixed(1),
-          ratingBggCount:
-            item.stats.rating.usersrated['@_value'].length > 3
-              ? Math.round(Math.floor(item.stats.rating.usersrated['@_value']) / 1000) + 'k'
-              : item.stats.rating.usersrated['@_value'],
-          comment: item.comment,
-          owned: item.status['@_own'] == '1',
-        }))
-        .filter((item) => item.owned);
+      const collection = collectionObj.items.item.map((item) => ({
+        id: item['@_objectid'],
+        image: item.image,
+        thumbnail: item.thumbnail,
+        name: String(item.name['#text']),
+        year: item.yearpublished,
+        players: {
+          min: Math.floor(item.stats['@_minplayers']),
+          max: Math.floor(item.stats['@_maxplayers']),
+        },
+        playTime: {
+          min: Math.floor(item.stats['@_minplaytime']),
+          max: Math.floor(item.stats['@_maxplaytime']),
+        },
+        rating: Math.floor(item.stats.rating['@_value']) || null,
+        ratingBgg: (Math.round(parseFloat(item.stats.rating.average['@_value']) * 10) / 10).toFixed(1),
+        ratingBggCount:
+          item.stats.rating.usersrated['@_value'].length > 3
+            ? Math.round(Math.floor(item.stats.rating.usersrated['@_value']) / 1000) + 'k'
+            : item.stats.rating.usersrated['@_value'],
+        comment: item.comment,
+        owned: item.status['@_own'] == '1',
+      }));
 
-      // Sort by rating then name
+      // Sort by ratings then name
       collection.sort((a, b) => b.rating - a.rating || b.ratingBgg - a.ratingBgg || a.name.localeCompare(b.name));
 
+      // Render to view
       this.items = collection;
-      this.loading = false;
       setTimeout(relayout, 10);
+
+      this.loading = false;
     },
   }));
 });
