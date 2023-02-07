f55e9d9712852163f059a11cf96f71b525f2e7e2
kputrajaya <freakperzon@yahoo.com>
2023-02-07 08:30:06 +0700

    Code smells

--- site.js
+++ site.js
@@ -18,6 +18,42 @@ document.addEventListener('alpine:init', () => {
     const collectionText = await fetchRes.text();
     return parser.parse(collectionText);
   };
+  const getCollection = async (username) => {
+    const collectionObj = await getData(
+      `collection/?username=${username}&own=1&excludesubtype=boardgameexpansion&stats=1`
+    );
+    const collection = collectionObj.items.item
+      .map((item) => ({
+        id: item['@_objectid'],
+        image: item.image.replace(/cf\.geekdo-images\.com/, 'ik.imagekit.io/kvn/bgg') + '?tr=w-800',
+        name: String(item.name['#text']),
+        year: item.yearpublished,
+        players: {
+          min: Math.floor(item.stats['@_minplayers']),
+          max: Math.floor(item.stats['@_maxplayers']),
+        },
+        playTime: {
+          min: Math.floor(item.stats['@_minplaytime']),
+          max: Math.floor(item.stats['@_maxplaytime']),
+        },
+        weight: null,
+        rating: Math.floor(item.stats.rating['@_value']) || null,
+        ratingBgg: Math.round(parseFloat(item.stats.rating.average['@_value']) * 10) / 10,
+        ratingBggCount:
+          item.stats.rating.usersrated['@_value'].length > 3
+            ? Math.round(Math.floor(item.stats.rating.usersrated['@_value']) / 1000) + 'k'
+            : item.stats.rating.usersrated['@_value'],
+        rank: Math.floor((item.stats.rating.ranks.rank[0] || item.stats.rating.ranks.rank)['@_value']),
+        comment: item.comment,
+        owned: item.status['@_own'] == '1',
+      }))
+      .sort((a, b) => b.rating - a.rating || a.rank - b.rank);
+    return collection;
+  };
+  const getThings = async (thingIds) => {
+    const thingObjs = await getData(`thing?id=${thingIds.join(',')}&stats=1&pagesize=100`);
+    return thingObjs.items.item;
+  };
   const relayout = () => {
     wrapper.reloadItems();
     wrapper.layout();
@@ -33,18 +69,17 @@ document.addEventListener('alpine:init', () => {
     // Data
     username: '',
     items: null,
-    filter: {
-      player: null,
-      weight: null,
-    },
+    filter: { player: null, weight: null },
     loading: false,
 
     // Computed
     itemsLength() {
-      return (this.items || []).length;
+      return this.items ? this.items.length : 0;
     },
     itemsSliced() {
-      let items = this.items || [];
+      if (!this.items) return [];
+
+      let items = this.items;
       if (this.filter.player) {
         const filterPlayer = Math.floor(this.filter.player);
         items = items.filter((item) => item.players.min <= filterPlayer && item.players.max >= filterPlayer);
@@ -60,7 +95,7 @@ document.addEventListener('alpine:init', () => {
 
       const counter = {};
       this.items.forEach((item) => {
-        for (i = item.players.min; i <= item.players.max && i <= this.filterPlayerMax; i++) {
+        for (let i = item.players.min; i <= item.players.max && i <= this.filterPlayerMax; i++) {
           counter[i] = (counter[i] || 0) + 1;
         }
       });
@@ -68,7 +103,7 @@ document.addEventListener('alpine:init', () => {
         .sort((a, b) => a - b)
         .map((count) => ({
           value: count,
-          text: `${count < this.filterPlayerMax ? count : `${this.filterPlayerMax}+`} player${count > 1 ? 's' : ''}
+          text: `${count < this.filterPlayerMax ? count : this.filterPlayerMax + '+'} player${count > 1 ? 's' : ''}
             (${this.formatNumber(counter[count])})`,
         }));
     },
@@ -91,61 +126,30 @@ document.addEventListener('alpine:init', () => {
     },
 
     // Method
+    formatNumber(num) {
+      return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    },
     init() {
       const params = new URLSearchParams(window.location.search);
       this.username = params.get('u') || '';
       if (!this.username) return;
       this.load();
     },
-    formatNumber(num) {
-      return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-    },
     async load() {
       this.loading = true;
 
-      const collectionObj = await getData(
-        `collection/?username=${this.username}&own=1&excludesubtype=boardgameexpansion&stats=1`
-      );
-      const collection = collectionObj.items.item
-        .map((item) => ({
-          id: item['@_objectid'],
-          image: item.image.replace(/cf\.geekdo-images\.com/, 'ik.imagekit.io/kvn/bgg') + '?tr=w-800',
-          name: String(item.name['#text']),
-          year: item.yearpublished,
-          players: {
-            min: Math.floor(item.stats['@_minplayers']),
-            max: Math.floor(item.stats['@_maxplayers']),
-          },
-          playTime: {
-            min: Math.floor(item.stats['@_minplaytime']),
-            max: Math.floor(item.stats['@_maxplaytime']),
-          },
-          weight: null,
-          rating: Math.floor(item.stats.rating['@_value']) || null,
-          ratingBgg: Math.round(parseFloat(item.stats.rating.average['@_value']) * 10) / 10,
-          ratingBggCount:
-            item.stats.rating.usersrated['@_value'].length > 3
-              ? Math.round(Math.floor(item.stats.rating.usersrated['@_value']) / 1000) + 'k'
-              : item.stats.rating.usersrated['@_value'],
-          rank: Math.floor((item.stats.rating.ranks.rank[0] || item.stats.rating.ranks.rank)['@_value']),
-          comment: item.comment,
-          owned: item.status['@_own'] == '1',
-        }))
-        .sort((a, b) => b.rating - a.rating || a.rank - b.rank);
-
-      // Render to view
-      this.items = collection;
+      this.items = await getCollection(this.username);
       setTimeout(relayout, 10);
 
       this.loading = false;
       this.enrich();
     },
     async enrich() {
-      const objectIds = this.items.map((item) => item.id);
+      const thingIds = this.items.map((item) => item.id);
       const chunkSize = 100;
-      for (let i = 0; i < objectIds.length; i += chunkSize) {
-        const thingObjs = await getData(`thing?id=${objectIds.slice(i, i + chunkSize).join(',')}&stats=1&pagesize=100`);
-        thingObjs.items.item.forEach((item, j) => {
+      for (let i = 0; i < thingIds.length; i += chunkSize) {
+        const objects = await getThings(thingIds.slice(i, i + chunkSize));
+        objects.forEach((item, j) => {
           this.items[i + j].weight = parseFloat(item.statistics.ratings.averageweight['@_value']) || null;
         });
       }
