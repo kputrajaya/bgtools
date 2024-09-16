073c3a589b90fcea0f597e90f3b2fe98ad33067a
Kevin Putrajaya <freakperzon@yahoo.com>
2024-09-16 17:33:54 +0700

    Incorporate dials and score

---
+++ dials/site.js
@@ -0,0 +1,204 @@
+document.addEventListener('alpine:init', () => {
+  const PRESETS = {
+    Gloomhaven: [
+      {
+        name: 'Players',
+        open: true,
+        items: [...Array(4).keys()].map((i) => ({
+          name: `P${i + 1}`,
+          metrics: [{ value: 10 }, { value: 0, context: 'info' }],
+        })),
+      },
+      ...[...Array(6).keys()].map((i) => ({
+        name: `Monster ${i + 1}`,
+        open: false,
+        items: [...Array(6).keys()].map((j) => ({ name: `${j + 1}`, metrics: [{ value: 0 }] })),
+      })),
+    ],
+    'Star Realms': [
+      {
+        name: 'Players',
+        open: true,
+        items: [...Array(2).keys()].map((i) => ({
+          name: `P${i + 1}`,
+          metrics: [{ value: 50 }],
+          size: 2,
+        })),
+      },
+      {
+        name: 'Players',
+        open: false,
+        items: [...Array(2).keys()].map((i) => ({
+          name: `P${i + 3}`,
+          metrics: [{ value: 50 }],
+          size: 2,
+        })),
+      },
+    ],
+    'Generic - 1 Value': [
+      {
+        name: 'Players',
+        open: true,
+        items: [...Array(4).keys()].map((i) => ({ name: `P${i + 1}`, metrics: [{ value: 0 }] })),
+      },
+      {
+        name: 'Players',
+        open: false,
+        items: [...Array(4).keys()].map((i) => ({ name: `P${i + 5}`, metrics: [{ value: 0 }] })),
+      },
+      {
+        name: 'Players',
+        open: false,
+        items: [...Array(4).keys()].map((i) => ({ name: `P${i + 9}`, metrics: [{ value: 0 }] })),
+      },
+    ],
+    'Generic - 2 Values': [
+      {
+        name: 'Players',
+        open: true,
+        items: [...Array(4).keys()].map((i) => ({
+          name: `P${i + 1}`,
+          metrics: [{ value: 0 }, { value: 0, context: 'info' }],
+        })),
+      },
+      {
+        name: 'Players',
+        open: false,
+        items: [...Array(4).keys()].map((i) => ({
+          name: `P${i + 5}`,
+          metrics: [{ value: 0 }, { value: 0, context: 'info' }],
+        })),
+      },
+      {
+        name: 'Players',
+        open: false,
+        items: [...Array(4).keys()].map((i) => ({
+          name: `P${i + 9}`,
+          metrics: [{ value: 0 }, { value: 0, context: 'info' }],
+        })),
+      },
+    ],
+    'Generic - 3 Values': [
+      {
+        name: 'Players',
+        open: true,
+        items: [...Array(4).keys()].map((i) => ({
+          name: `P${i + 1}`,
+          metrics: [{ value: 0 }, { value: 0, context: 'warning' }, { value: 0, context: 'info' }],
+        })),
+      },
+      {
+        name: 'Players',
+        open: false,
+        items: [...Array(4).keys()].map((i) => ({
+          name: `P${i + 5}`,
+          metrics: [{ value: 0 }, { value: 0, context: 'warning' }, { value: 0, context: 'info' }],
+        })),
+      },
+      {
+        name: 'Players',
+        open: false,
+        items: [...Array(4).keys()].map((i) => ({
+          name: `P${i + 9}`,
+          metrics: [{ value: 0 }, { value: 0, context: 'warning' }, { value: 0, context: 'info' }],
+        })),
+      },
+    ],
+  };
+
+  const deepCopy = (obj) => JSON.parse(JSON.stringify(obj));
+  const getParams = () => {
+    const params = {};
+    const search = window.location.search;
+    if (search) {
+      search
+        .substring(1)
+        .split('&')
+        .forEach((param) => {
+          const [key, value] = param.split('=');
+          params[key] = decodeURIComponent(value).replace(/\+/g, ' ').replace(/\|/g, '\n');
+        });
+    }
+    return params;
+  };
+
+  Alpine.data('dials', function () {
+    return {
+      // Constants
+      presetKeys: Object.keys(PRESETS),
+
+      // Data
+      categories: this.$persist([]),
+      lastJson: null,
+
+      // Method
+      itemActive(item) {
+        return item.metrics.some((metric) => metric.value > 0);
+      },
+      loadPreset(presetKey) {
+        this.categories = deepCopy(PRESETS[presetKey]);
+      },
+      metricDecrease(metric) {
+        if (metric.value > 0) {
+          metric.value--;
+        } else {
+          metric.value = 0;
+        }
+      },
+      metricIncrease(metric) {
+        metric.value++;
+      },
+      metricBlur(metric) {
+        const safeValue = Math.max(0, Math.floor(metric.value) || 0);
+        metric.value = null;
+        metric.value = safeValue;
+      },
+      reset() {
+        const response = prompt('Reset values? Type "y" to continue.') || '';
+        if (response.trim().toLowerCase() !== 'y') return;
+        this.categories = [];
+      },
+
+      // Initialization
+      init() {
+        const connect = (subKey) => {
+          const ws = new WebSocket('wss://pubsub.h.kvn.pt/');
+          ws.onopen = () => {
+            console.log('Subscribing to:', subKey);
+            ws.send(JSON.stringify({ action: 'sub', key: subKey }));
+          };
+          ws.onmessage = (event) => {
+            console.log('Received data');
+            this.lastJson = event.data;
+            this.categories = JSON.parse(event.data);
+          };
+          this.$watch('categories', (value) => {
+            const currentJson = JSON.stringify(value);
+            if (currentJson === this.lastJson) return;
+            this.lastJson = currentJson;
+            console.log('Sending data');
+            ws.send(JSON.stringify({ action: 'pub', key: subKey, data: value }));
+          });
+          const interval = setInterval(() => {
+            console.log('Sending data (interval)');
+            ws.send(JSON.stringify({ action: 'pub', key: subKey, data: this.categories }));
+          }, 60000);
+          ws.onerror = function (err) {
+            console.error('Socket error:', err.message);
+            ws.close();
+          };
+          ws.onclose = (e) => {
+            console.log('Socket closed:', e.reason);
+            setTimeout(() => connect(subKey), 1000);
+            clearInterval(interval);
+          };
+        };
+
+        const params = getParams();
+        if (params.k) {
+          connect('dials:' + params.k);
+        }
+      },
+    };
+  });
+});
---
+++ score/site.js
@@ -0,0 +1,120 @@
+(() => {
+  const gameData = {
+    Earth: [
+      'Base Point',
+      'Event',
+      'Compost',
+      'Sprout',
+      'Growth',
+      'Terrain Bonus',
+      'Shared Ecosystem',
+      'Personal Ecosystem',
+      'Fauna',
+      'First to Complete',
+    ],
+  };
+
+  const tableContainer = document.getElementById('scoreTable');
+  let table,
+    numPlayers = 1;
+
+  function createElm(tag, className, attributes = {}) {
+    const element = document.createElement(tag);
+    element.className = className;
+    Object.entries(attributes).forEach(([key, value]) => (element[key] = value));
+    return element;
+  }
+
+  function createInput(type, className, value, onfocus, onblur) {
+    return createElm('input', className, { type, value, onfocus, onblur });
+  }
+
+  function createScoreRow(numPlayers, categoryName = '') {
+    const row = document.createElement('tr');
+    row
+      .appendChild(createElm('td'))
+      .appendChild(createInput('text', 'p-2 text-start', categoryName, (e) => e.target.select()));
+    for (let i = 1; i <= numPlayers; i++) {
+      row
+        .appendChild(createElm('td'))
+        .appendChild(createInput('tel', 'p-2 text-end', '0', (e) => e.target.select(), updateTotals));
+    }
+    return row;
+  }
+
+  function createTotalRow(numPlayers) {
+    const row = document.createElement('tr');
+    row.appendChild(createElm('th', 'p-2 text-start')).innerText = 'Total';
+    for (let i = 1; i <= numPlayers; i++) {
+      row.appendChild(createElm('th', 'p-2 text-end')).innerText = '0';
+    }
+    return row;
+  }
+
+  function generateTable() {
+    table = createElm('table', 'table');
+    const headerRow = document.createElement('tr');
+    headerRow.appendChild(createElm('th', 'p-2')).innerText = 'Category';
+    for (let i = 1; i <= numPlayers; i++) {
+      headerRow.appendChild(createElm('th', 'p-2 text-end')).innerText = `P${i}`;
+    }
+    table.appendChild(headerRow);
+    for (let i = 0; i < 3; i++) {
+      table.appendChild(createScoreRow(numPlayers, `Category ${i + 1}`));
+    }
+    table.appendChild(createTotalRow(numPlayers));
+    tableContainer.innerHTML = '';
+    tableContainer.appendChild(table);
+  }
+
+  function updateTotals() {
+    const numRows = table.rows.length;
+    for (let player = 1; player <= numPlayers; player++) {
+      const sum = Array.from(table.rows)
+        .slice(1, -1)
+        .reduce((acc, row) => {
+          const input = row.cells[player].firstChild;
+          const value = parseInt(input.value) || 0;
+          input.value = value;
+          return acc + value;
+        }, 0);
+      table.rows[numRows - 1].cells[player].innerText = sum;
+    }
+  }
+
+  function loadCategories() {
+    const gameList = Object.keys(gameData);
+    const promptOptions = gameList.map((game, index) => `${index + 1}. ${game}`).join('\n');
+    const selectedGameIndex = Math.floor(prompt(`Load categories for a game:\n${promptOptions}`));
+    if (selectedGameIndex && selectedGameIndex > 0 && selectedGameIndex <= gameList.length) {
+      const categories = gameData[gameList[selectedGameIndex - 1]];
+      while (table.rows.length > 2) table.deleteRow(1);
+      categories.forEach((category) => table.insertBefore(createScoreRow(numPlayers, category), table.lastChild));
+      updateTotals();
+    }
+  }
+
+  function addRow() {
+    const numCategories = table.rows.length - 2;
+    if (numCategories >= 20) return;
+    table.insertBefore(createScoreRow(numPlayers, `Category ${numCategories + 1}`), table.lastChild);
+  }
+
+  function addColumn() {
+    if (numPlayers >= 20) return;
+    numPlayers++;
+    table.rows[0].appendChild(createElm('th', 'p-2 text-end')).innerText = `P${numPlayers}`;
+    for (let i = 1; i < table.rows.length - 1; i++) {
+      table.rows[i]
+        .appendChild(createElm('td'))
+        .appendChild(createInput('tel', 'p-2 text-end', '0', (e) => e.target.select(), updateTotals));
+    }
+    table.rows[table.rows.length - 1].appendChild(createElm('th', 'total p-2 text-end')).innerText = '0';
+    updateTotals();
+  }
+
+  generateTable();
+  document.getElementById('loadBtn').addEventListener('click', loadCategories);
+  document.getElementById('addRowBtn').addEventListener('click', addRow);
+  document.getElementById('addColumnBtn').addEventListener('click', addColumn);
+})();
