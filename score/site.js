9360b04d88153f884d25a4563fe5678907ea31eb
Kevin Putrajaya <freakperzon@yahoo.com>
2025-01-05 07:18:35 +0700

    Refactor and reduce code smells

--- score/site.js
+++ score/site.js
@@ -1,8 +1,13 @@
 (async () => {
-  const tableContainer = document.getElementById('scoreTable');
-  let gameData = {};
-  let table;
-  let numPlayers = 2;
+  const MAX_CATEGORIES = 20;
+  const MAX_PLAYERS = 20;
+  const global = {
+    tableContainer: document.getElementById('scoreTable'),
+    gameData: { '': ['Category 1', 'Category 2', 'Category 3'] },
+    table: null,
+    numPlayers: 2,
+    debounceTimeout: null,
+  };
 
   function createElm(tag, className = '', attributes = {}) {
     const element = document.createElement(tag);
@@ -18,39 +23,40 @@
       onfocus: (e) => e.target.select(),
     };
     if (handler) {
-      let timeout;
-      const debouncedHandler = () => {
-        clearTimeout(timeout);
-        timeout = setTimeout(() => handler(this), 1000);
+      attributes.onblur = () => {
+        clearTimeout(global.debounceTimeout);
+        handler();
       };
-      attributes.onblur = handler;
-      attributes.onkeyup = debouncedHandler;
+      attributes.onkeyup = () => {
+        clearTimeout(global.debounceTimeout);
+        global.debounceTimeout = setTimeout(() => handler(), 1000);
+      };
     }
     return createElm('input', className, attributes);
   }
 
-  function createScoreRow(numPlayers, categoryName = '') {
+  function createScoreRow(categoryName = '') {
     const row = createElm('tr');
-    row.appendChild(createElm('td')).appendChild(createInput('text', 'p-2 fs-7 text-start', categoryName));
-    for (let i = 1; i <= numPlayers; i++) {
+    row.appendChild(createElm('td')).appendChild(createInput('text', 'p-2 text-start', categoryName));
+    for (let i = 1; i <= global.numPlayers; i++) {
       row.appendChild(createElm('td')).appendChild(createInput('tel', 'p-2 text-end', '0', updateTotals));
     }
     return row;
   }
 
-  function createTotalRow(numPlayers) {
+  function createTotalRow() {
     const row = createElm('tr');
-    row.appendChild(createElm('th', 'p-2 fs-7 text-start')).innerText = 'Total';
-    for (let i = 1; i <= numPlayers; i++) {
+    row.appendChild(createElm('th', 'p-2 text-start')).innerText = 'Total';
+    for (let i = 1; i <= global.numPlayers; i++) {
       row.appendChild(createElm('th', 'p-2 text-end')).innerText = '0';
     }
     return row;
   }
 
   function updateTotals() {
-    const numRows = table.rows.length;
-    for (let player = 1; player <= numPlayers; player++) {
-      const sum = Array.from(table.rows)
+    const numRows = global.table.rows.length;
+    for (let player = 1; player <= global.numPlayers; player++) {
+      const sum = Array.from(global.table.rows)
         .slice(1, -1)
         .reduce((acc, row) => {
           const input = row.cells[player].firstChild;
@@ -58,62 +64,70 @@
           input.value = value;
           return acc + value;
         }, 0);
-      table.rows[numRows - 1].cells[player].innerText = sum;
+      global.table.rows[numRows - 1].cells[player].innerText = sum;
     }
   }
 
   async function initialize() {
+    // Create table
+    global.table = createElm('table', 'table fs-7');
+    const headerRow = createElm('tr');
+    headerRow.appendChild(createElm('th', 'p-2')).innerText = 'Category';
+    for (let i = 1; i <= global.numPlayers; i++) {
+      headerRow.appendChild(createElm('th', 'p-2 text-end')).innerText = `P${i}`;
+    }
+    global.table.appendChild(headerRow);
+    global.table.appendChild(createTotalRow());
+    global.tableContainer.innerHTML = '';
+    global.tableContainer.appendChild(global.table);
+    loadCategories('');
+
     // Load game data
-    const response = await fetch(
-      'https://gist.githubusercontent.com/kputrajaya/1e0d9e787c7716a05199659fb42d3ba5/raw/bgtools-score.json'
-    );
-    gameData = await response.json();
+    try {
+      const response = await fetch(
+        'https://gist.githubusercontent.com/kputrajaya/1e0d9e787c7716a05199659fb42d3ba5/raw/bgtools-score.json'
+      );
+      global.gameData = Object.assign(global.gameData, await response.json());
+    } catch (error) {
+      alert('Failed to load game data');
+    }
 
     // Create dropdown
     const gameSelect = document.getElementById('gameSelect');
-    Object.keys(gameData).forEach((game) => {
+    Object.keys(global.gameData).forEach((game) => {
       if (!game) return;
       const option = createElm('option');
       option.value = game;
       option.textContent = game;
       gameSelect.appendChild(option);
     });
-
-    // Create table
-    table = createElm('table', 'table');
-    const headerRow = createElm('tr', 'fs-7');
-    headerRow.appendChild(createElm('th', 'p-2')).innerText = 'Category';
-    for (let i = 1; i <= numPlayers; i++) {
-      headerRow.appendChild(createElm('th', 'p-2 text-end')).innerText = `P${i}`;
-    }
-    table.appendChild(headerRow);
-    table.appendChild(createTotalRow(numPlayers));
-    tableContainer.innerHTML = '';
-    tableContainer.appendChild(table);
-    loadCategories('');
   }
 
   function loadCategories(selectedGame) {
-    const categories = gameData[selectedGame];
-    while (table.rows.length > 2) table.deleteRow(1);
-    categories.forEach((category) => table.insertBefore(createScoreRow(numPlayers, category), table.lastChild));
+    const categories = global.gameData[selectedGame];
+    while (global.table.rows.length > 2) global.table.deleteRow(1);
+    categories.forEach((category) => global.table.insertBefore(createScoreRow(category), global.table.lastChild));
     updateTotals();
   }
 
   function addRow() {
-    const numCategories = table.rows.length - 2;
-    if (numCategories >= 20) return;
-    table.insertBefore(createScoreRow(numPlayers, `Category ${numCategories + 1}`), table.lastChild);
+    const numCategories = global.table.rows.length - 2;
+    if (numCategories >= MAX_CATEGORIES) return;
+
+    global.table.insertBefore(createScoreRow(`Category ${numCategories + 1}`), global.table.lastChild);
   }
 
   function addColumn() {
-    if (numPlayers >= 20) return;
-    numPlayers++;
-    table.rows[0].appendChild(createElm('th', 'p-2 text-end')).innerText = `P${numPlayers}`;
-    for (let i = 1; i < table.rows.length - 1; i++) {
-      table.rows[i].appendChild(createElm('td')).appendChild(createInput('tel', 'p-2 text-end', '0', updateTotals));
+    if (global.numPlayers >= MAX_PLAYERS) return;
+
+    global.numPlayers++;
+    global.table.rows[0].appendChild(createElm('th', 'p-2 text-end')).innerText = `P${global.numPlayers}`;
+    for (let i = 1; i < global.table.rows.length - 1; i++) {
+      global.table.rows[i]
+        .appendChild(createElm('td'))
+        .appendChild(createInput('tel', 'p-2 text-end', '0', updateTotals));
     }
-    table.rows[table.rows.length - 1].appendChild(createElm('th', 'total p-2 text-end')).innerText = '0';
+    global.table.rows[global.table.rows.length - 1].appendChild(createElm('th', 'total p-2 text-end')).innerText = '0';
     updateTotals();
   }
 
