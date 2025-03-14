0bdf98e02333d69389078dc88c8d713023683c2e
Kevin Putrajaya <freakperzon@yahoo.com>
2025-03-14 22:26:15 +0700

    Move init below for scores

--- score/site.js
+++ score/site.js
@@ -11,18 +11,6 @@ document.addEventListener('alpine:init', () => {
       { name: 'P2', scores: [0, 0] },
     ],
 
-    async init() {
-      try {
-        const response = await fetch(
-          'https://gist.githubusercontent.com/kputrajaya/1e0d9e787c7716a05199659fb42d3ba5/raw/bgtools-score.json'
-        );
-        const gameData = await response.json();
-        Object.assign(this.gameData, gameData);
-      } catch (error) {
-        alert('Failed to load game presets!');
-      }
-    },
-
     addCategory() {
       if (this.categories.length >= MAX_CATEGORIES) return;
       this.categories.push(`Category ${this.categories.length + 1}`);
@@ -51,6 +39,19 @@ document.addEventListener('alpine:init', () => {
     },
     select() {
       this.$el.select();
+    },
+
+    // Initialization
+    async init() {
+      try {
+        const response = await fetch(
+          'https://gist.githubusercontent.com/kputrajaya/1e0d9e787c7716a05199659fb42d3ba5/raw/bgtools-score.json'
+        );
+        const gameData = await response.json();
+        Object.assign(this.gameData, gameData);
+      } catch (error) {
+        alert('Failed to load game presets!');
+      }
     },
   }));
 });
